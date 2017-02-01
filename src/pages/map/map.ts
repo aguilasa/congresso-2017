9cd4873a87bd7be83c4fbc2c932a8ec09288cd02
Ingmar.Aguiar <ingmar.aguiar@senior.com.br>
2017-02-01 19:32:55 -0200

    Outras + Cifras

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -6,41 +6,10 @@ import { NavController, Slides } from 'ionic-angular';
   templateUrl: 'map.html'
 })
 export class MapPage {
-  @ViewChild('mySlider') slider: Slides;
-  selectedSegment: string;
-  slides: any;
+
 
   constructor(public navCtrl: NavController) {
-    this.selectedSegment = 'first';
-    this.slides = [
-      {
-        id: "first",
-        title: "First Slide"
-      },
-      {
-        id: "second",
-        title: "Second Slide"
-      },
-      {
-        id: "third",
-        title: "Third Slide"
-      }
-    ];
-  }
 
-  onSegmentChanged(segmentButton) {
-    console.log("Segment changed to", segmentButton.value);
-    const selectedIndex = this.slides.findIndex((slide) => {
-      return slide.id === segmentButton.value;
-    });
-    this.slider.slideTo(selectedIndex);
-  }
-
-  onSlideChanged() {
-    console.log('Slide changed');
-    
-    const currentSlide = this.slides[this.slider.getActiveIndex()];
-    this.selectedSegment = currentSlide.id;
   }
 
 }
