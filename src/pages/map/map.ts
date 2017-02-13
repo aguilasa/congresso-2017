fc31ac19b3cbcbfbc737846b95635ccfb71d790f
Ingmar.Aguiar <ingmar.aguiar@senior.com.br>
2017-02-13 18:51:16 -0200

    Mapa

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import Raphael from 'raphael';
 
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html'
 })
 export class MapPage {
-
+  @ViewChild('container')
+  container: ElementRef;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    console.log(this.container);
   }
 
 }
